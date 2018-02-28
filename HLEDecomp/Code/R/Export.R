2875c57ee3e37f726fe5b04cdce3b23149a9e6a6
tim riffe <tim.riffe@gmail.com>
2018-02-28 10:12:49 +0100

    export transition matrix

---
+++ HLEDecomp/Code/R/Export.R
@@ -0,0 +1,16 @@
+
+# just to make a transition matrix for Transient Symmetry
+
+me <- system("whoami",intern=TRUE)
+if (me == "mpidr_d\\riffe"){
+	setwd("U:/git/HLEDecomp/HLEDecomp")
+}
+if (me == "tim"){
+	setwd("/home/tim/git/HLEDecomp/HLEDecomp")
+}
+source("Code/R/Functions.R")
+
+
+dat <- get_data()
+str(dat)
+U <- data_2_U(dat)
