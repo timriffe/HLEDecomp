62b39549e6e1de9944ba3437c777e9c1e9209890
Tim Riffe <tim.riffe@gmail.com>
2018-05-09 12:53:24 -0400

    rather than having these lines at the top of every script...

---
+++ HLEDecomp/Code/R/Preamble.R
@@ -0,0 +1,11 @@
+
+# Author: tim
+###############################################################################
+# stuff that was running at the top of every script anyway...
+sexes        <- c("m", "f", "b")
+edus         <- c("all_edu", "primary" , "secondary", "terciary"  )
+edusl        <- c("0.All edu", "1.Less HS" , "4.HS/GED/Sm coll ex AA", "5.AA/BS/+"  )
+names(edusl) <- edus
+N            <- 20
+versions     <- c("01","02","03")
+
