5a70f87018ef874d1ea6478dc427c03f8d668538
Tim Riffe <tim.riffe@gmail.com>
2018-05-09 15:05:55 -0400

    finally get things in Native R off the bat

--- HLEDecomp/Code/R/ReadTR.R
+++ HLEDecomp/Code/R/ReadTR.R
@@ -16,13 +16,20 @@ if (me == "tim"){
 source("Code/R/Functions.R")
 source("Code/R/Preamble.R")
 #
-names(edus) <- edusl
-sexes
-for (i in length(versions)){
+
+for (i in 1:length(versions)){
 	TR.i <- get_rates_all(
-			path = read.path, 
-			version = versions[i],
-			self = TRUE)
+			  path = read.path, 
+			  version = versions[i],
+			  self = TRUE)
 	TR.i$edu <- edus[TR.i$educlevel]
+	TR.i$Sex <- TR.i$sex
+	TR.i$sex <- sexes[TR.i$sex]
+	
+	save(TR.i, file = file.path("Data","Transitions","DCS",paste0("TR_v",versions[i],".Rdata")))
 }
+subset(TR.i, sex == "f" & edu == "terciary" & time == 2006)
+
+
+
 
